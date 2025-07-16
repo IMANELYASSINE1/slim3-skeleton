8e85e4e8a3e8dc7ac29c0aa7b4c17df15c50b55b
IMANELYASSINE1 <elyassiniman2017@gmail.com>
2025-07-16 12:48:32 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,22 +6,22 @@ if (PHP_SAPI === 'cli-server' && $_SERVER['SCRIPT_FILENAME'] !== __FILE__) {
     return false;
 }
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 session_start();
 
 // Instantiate the app
-$settings = require_once __DIR__ . '/../app/settings.php';
+$settings = require __DIR__ . '/../app/settings.php';
 $app = new \Slim\App($settings);
 
 // Set up dependencies
-require_once __DIR__ . '/../app/dependencies.php';
+require __DIR__ . '/../app/dependencies.php';
 
 // Register middleware
-require_once __DIR__ . '/../app/middleware.php';
+require __DIR__ . '/../app/middleware.php';
 
 // Register routes
-require_once __DIR__ . '/../app/routes.php';
+require __DIR__ . '/../app/routes.php';
 
 // Run!
 $app->run();
